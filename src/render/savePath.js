6f6ceabf8425a3b3cb4d47024de28dd424cfbdb2
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-08-03 00:16:05 +0200

    added save.json to findSavePath()

--- src/render/savePath.js
+++ src/render/savePath.js
@@ -3,13 +3,13 @@ const path = require('path');
 function findSavePath () {
 	switch(process.platform) {
 		case 'darwin': {
-		  return path.join(process.env.HOME, 'Library', 'Application Support', 'frs');
+		  return path.join(process.env.HOME, 'Library', 'Application Support', 'frs', 'save.json');
 		}
 		case 'win32': {
-		  return path.join(process.env.APPDATA, 'frs');
+		  return path.join(process.env.APPDATA, 'frs', 'save.json');
 		}
 		case 'linux': {
-		  return path.join(process.env.HOME, 'frs');
+		  return path.join(process.env.HOME, 'frs', 'save.json');
 		}
 	}
 };
