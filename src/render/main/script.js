0cc09027fff2f9fd49bc99da7f213a119e7057e1
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-08-03 17:01:40 +0200

    deleted unnecessary files

--- src/render/main/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-	$('button').click(function() {
-		if (['session','cards','flashcards'].includes(this.id)) {
-			location.href = `../${this.id}/index.html`;
-		}
-	})
-})
